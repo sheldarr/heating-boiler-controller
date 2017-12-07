2560db045e7fc24060e0fd8e4061ff0ef9e02cea
sheldar <kewin.polok@gmail.com>
2017-12-07 19:44:17 +0100

    Basic engine module

---
+++ engine.lua
@@ -0,0 +1,17 @@
+engine = {};
+
+function loop()
+    print("Loop");
+end
+
+engine.start = function (interval)
+    result = tmr.create():alarm(interval, tmr.ALARM_SINGLE, loop)
+    if result
+    then
+        print("Engine started")
+    else
+        print("Engine error")
+    end
+end
+
+return engine;
