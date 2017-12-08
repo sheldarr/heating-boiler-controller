f8f6a3499d694f17dc3b9a4528a0f99cf2ae5ca9
sheldar <kewin.polok@gmail.com>
2017-12-08 18:31:23 +0100

    Extracted server module

--- init.lua
+++ init.lua
@@ -9,66 +9,11 @@ dofile('buzzer.lua');
 dofile('fan.lua');
 dofile('sensor.lua');
 dofile('engine.lua');
+dofile('server.lua');
 
 engine.start(1000);
+server.start();
 
 buzzer.beep();
-
-wifi.setmode(wifi.STATION)
-
-ipConfig = {
-    ip = "192.168.0.164",
-    netmask = "255.255.255.0",
-    gateway = "192.168.0.1"
-}
-
-wifi.sta.setip(ipConfig)
-
-stationConfig = {
-    ssid = "",
-    pwd = ""
-}
-
-wifi.sta.config(stationConfig)
-
-server = net.createServer(net.TCP, 30)
-
-function onReceive(socket, data)
-    print(data)
-
-    ds18b20.read(
-        function(index, rom, resolution, temperature)
-            beep()
-            local message = string.format(
-                "Index: %d Resolution: %d Temperature: %f °C",
-                index,
-                resolution,
-                temperature
-            )
-            
-            print(message)
-            
-            fixedTemperature = string.format(
-                "%.4f",
-                temperature
-            );
-
-            socket:send("HTTP/1.1 200 OK\n")
-            socket:send("Server: ESP8266 (nodemcu)\n")
-            socket:send(string.format(
-                "Content-Length: %i\n\n",
-                fixedTemperature:len())
-            )
-            socket:send(fixedTemperature)
-        end,
-        {}
-    )
-end
-
-if server then
-    server:listen(80, function(socket)
-        socket:on("receive", onReceive)
-    end)
-end
 
 print(wifi.sta.getip())
