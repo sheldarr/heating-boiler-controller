b291adbf78baadbece891cb6c27784acccbdf531
sheldar <kewin.polok@gmail.com>
2017-12-12 20:56:18 +0100

    Buzzer module refactor

--- buzzer.lua
+++ buzzer.lua
@@ -1,13 +1,13 @@
-BUZZER_PIN = 6
-BUZZER_LENGTH = 500000
+local PIN = 6
+local LENGTH = 500000
 
-gpio.mode(BUZZER_PIN, gpio.OUTPUT)
+gpio.mode(PIN, gpio.OUTPUT)
 
 buzzer = {}
 
 buzzer.beep = function(length)
-    buzzLength = length or BUZZER_LENGTH
-    gpio.serout(BUZZER_PIN, gpio.LOW, {BUZZER_LENGTH, BUZZER_LENGTH})
+    length = length or LENGTH
+    gpio.serout(PIN, gpio.LOW, {length, length})
 end
 
 return buzzer
