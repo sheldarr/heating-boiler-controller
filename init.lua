6a8632044d96812db7a5d1c38fbcb923c99a4d20
sheldar <kewin.polok@gmail.com>
2021-02-03 19:41:28 +0100

    Replace config with state

--- init.lua
+++ init.lua
@@ -1,11 +1,2 @@
-local led = require('led');
-
-local engine = require('engine');
-local server = require('server');
-
-engine.start(5000);
-server.start();
-
-led.blink();
-
-print(wifi.sta.getip())
+require('engine');
+require('server');
