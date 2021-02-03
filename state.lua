9a34fa9da80c174e4b41343c7497b5eb1705a437
sheldar <kewin.polok@gmail.com>
2021-02-03 19:39:38 +0100

    Replace config with state

---
+++ state.lua
@@ -0,0 +1,43 @@
+local state = {}
+
+local getState = function()
+    local freshState = {}
+
+    if file.open('state') then
+        freshState.mode = file.readline():gsub("%s+", "")
+        freshState.hysteresis = tonumber(file.readline())
+        freshState.setpoint = tonumber(file.readline())
+        freshState.outputTemperature = tonumber(file.readline())
+        freshState.inputTemperature = tonumber(file.readline())
+
+        file.close()
+    end
+
+    return freshState
+end
+
+local setState = function(newState)
+    local previousState = getState()
+
+    newState.mode = newState.mode or previousState.mode
+    newState.hysteresis = newState.hysteresis or previousState.hysteresis
+    newState.setpoint = newState.setpoint or previousState.setpoint
+    newState.outputTemperature = newState.outputTemperature or
+                                     previousState.outputTemperature
+    newState.inputTemperature = newState.inputTemperature or
+                                    previousState.inputTemperature
+
+    if file.open('state', 'w+') then
+        file.writeline(newState.mode)
+        file.writeline(newState.hysteresis)
+        file.writeline(newState.setpoint)
+        file.writeline(newState.outputTemperature)
+        file.writeline(newState.inputTemperature)
+        file.close()
+    end
+end
+
+state.getState = getState
+state.setState = setState
+
+return state
